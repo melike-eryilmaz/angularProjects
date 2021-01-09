f2e1751b6e9e72cea32f5598f05e649215b7eb61
melike-eryilmaz <melikeeryilmaz1@gmail.com>
2021-01-09 19:04:18 +0300

    Login,Logout eklendi.

--- shopProject/src/app/app.component.ts
+++ shopProject/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CategoryComponent } from './category/category.component';
+import { AccountService } from './services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,16 @@ import { CategoryComponent } from './category/category.component';
   providers:[CategoryComponent]
 })
 export class AppComponent {
+  constructor(private accountService :AccountService){
+
+  }
   title = 'shopProject';
+
+  isLoggedIn(){
+    return this.accountService.isLoggedIn();
+  }
+  logOut(){
+    this.accountService.logOut();
+  }
+
 }
